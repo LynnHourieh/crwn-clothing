301cf54b71caa52ce0abc3258f633dda11e12322
lynnhourieh <lynnhourieh30@gmail.com>
2023-06-16 20:48:57 +0300

    fix error

--- crwn-project/src/main.jsx
+++ crwn-project/src/main.jsx
@@ -7,7 +7,7 @@ import { UserProvider } from "./contexts/user.context.jsx";
 import { CategoriesProvider } from "./contexts/categories.context.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React.StrictMode >
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
