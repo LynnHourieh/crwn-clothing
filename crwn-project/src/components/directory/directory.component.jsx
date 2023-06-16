fe2529707fd6c4f1c6d734a1c6183a1d4848ff82
lynnhourieh <lynnhourieh30@gmail.com>
2023-06-16 18:47:29 +0300

    last touches

--- crwn-project/src/components/directory/directory.component.jsx
+++ crwn-project/src/components/directory/directory.component.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import { DirectoryContainer } from "./directory.styles.jsx";
 import DirectoryItem from "../directory-item/directory-item.component.jsx";
 
-export const Directory = ({ categories }) => {
+export const Directory = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'hats',
+      imageurl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'jackets',
+      imageurl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'sneakers',
+      imageurl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'womens',
+      imageurl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'mens',
+      imageurl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
   return (
     <DirectoryContainer>
       {categories.map((category) => (
